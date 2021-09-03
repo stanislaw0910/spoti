4f2b53c21d3c62b1b096bd720fb935d96330bcf6
stanislaw0910 <kazabian666@gmail.com>
2021-09-03 15:43:39 +0300

    mp3 to string modified

--- mp3_to_string.py
+++ mp3_to_string.py
@@ -2,6 +2,18 @@ import os
 import glob
 import eyed3
 
+
+# this script takes main information from mp3 file tags and create tuple of strings for using in DB
+
+
+def duration_from_seconds(s):
+    s = s
+    m, s = divmod(s, 60)
+    timelapse = "{:01d}:{:02d}".format(int(m),
+                                       int(s))
+    return timelapse
+
+
 os.chdir(os.path.join(os.getcwd(), '2009_-_Crack_The_Skye/CD_1'))
 path = os.getcwd()
 '''filenames = os.listdir(path)
@@ -14,21 +26,20 @@ for file in glob.glob("*.jpg"):
 for file in glob.glob("*.mp3"):
     mp3_dir.append((file, os.path.abspath(file)))
 mp3_dir.sort()
-i = 0
 for name in mp3_dir:
     audiofile = eyed3.load(name[0])
     eyed3.log.setLevel("ERROR")
-    duration = str(int(audiofile.info.time_secs // 60)) + ':' + str(int(audiofile.info.time_secs % 60)) #calculating duration from seconds
+    duration = audiofile.info.time_secs
     mp3_data = (
-        artist, album, title, track_num, duration, genre, recording_date, path,cover, ) = audiofile.tag.artist, \
-                                                                        audiofile.tag.album, \
-                                                                        audiofile.tag.title, \
-                                                                        audiofile.tag.track_num[0], \
-                                                                        duration, \
-                                                                        tuple(str(audiofile.tag.genre).split(' / '))[0], \
-                                                                        str(audiofile.tag.recording_date),\
-                                                                        name[1], \
-                                                                        audiofile.tag.images[0].image_data,\
-
+        artist, album, title, track_num, duration, genre,
+        recording_date, path, cover,) = audiofile.tag.artist, \
+                                        audiofile.tag.album, \
+                                        audiofile.tag.title, \
+                                        audiofile.tag.track_num[0], \
+                                        duration_from_seconds(duration), \
+                                        tuple(str(audiofile.tag.genre).split(' / '))[0], \
+                                        str(audiofile.tag.recording_date), \
+                                        name[1], \
+                                        audiofile.tag.images[0].image_data, \
 
     print(mp3_data[:-1])
